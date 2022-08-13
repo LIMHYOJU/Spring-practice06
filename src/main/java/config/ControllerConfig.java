345c1839939baff6cad6c7024079b67e061dae48
HyojuLim <dlagywn94@gmail.com>
2022-08-13 17:57:31 +0900

    RequestMapping/요청피라미터/리다이렉트/폼태그/모델 연습

--- src/main/java/config/ControllerConfig.java
+++ src/main/java/config/ControllerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import controller.RegisterController;
+import controller.SurveyController;
 
 @Configuration
 public class ControllerConfig {
@@ -11,5 +12,10 @@ public class ControllerConfig {
 	@Bean
 	public RegisterController registerController() {
 		return new RegisterController();
+	}
+	
+	@Bean
+	public SurveyController surveyController() {
+		return new SurveyController();
 	}
 }
--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -1,13 +1,46 @@
 package controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import springTest.DuplicateMemberException;
+import springTest.MemberRegisterService;
+import springTest.RegisterRequest;
 
 @Controller
 public class RegisterController {
+	@Autowired
+	private MemberRegisterService memberRegisterService;
 	
 	@RequestMapping("/register/step1")
 	public String handleStep1() {
 		return "register/step1";
+	}
+	@PostMapping("/register/step2")
+	public String handleStep2(@RequestParam(value = "agree" ,defaultValue = "false") boolean agree,  Model model) {
+		if (!agree) {
+			return "register/step1";
+		}
+		model.addAttribute("formData", new RegisterRequest());
+		return "register/step2";
+	}
+	
+	@PostMapping("/register/step3")
+	public String handleStep3(@ModelAttribute("formData") RegisterRequest regReq) {
+		try {
+			memberRegisterService.regist(regReq);
+			return "/register/step3";
+		} catch (DuplicateMemberException ex) {
+			// TODO: handle exception
+			return "/register/step2";
+		}
+		
 	}
 }
